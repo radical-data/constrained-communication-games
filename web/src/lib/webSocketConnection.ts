88512f5e9217e03515f28fecd93217d72801e6ff
Jo Kroese <jo@jokroese.com>
2024-07-22 01:27:53 +0200

    use docker network and environment variables

--- web/src/lib/webSocketConnection.ts
+++ web/src/lib/webSocketConnection.ts
@@ -1,5 +1,5 @@
 import ioClient from 'socket.io-client';
-const ENDPOINT = 'http://localhost:3001/';
+const ENDPOINT = process.env.WS_ENDPOINT || 'http://localhost:3001/';
 
 const socket = ioClient(ENDPOINT);
 
