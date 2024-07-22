c36c57f4889b93a5a94361239fa808a558869d29
Jo Kroese <jo@jokroese.com>
2024-07-22 02:07:41 +0200

    use http for localhost

--- web/src/lib/webSocketConnection.ts
+++ web/src/lib/webSocketConnection.ts
@@ -1,5 +1,5 @@
 import ioClient from 'socket.io-client';
-const ENDPOINT = process.env.WS_ENDPOINT || 'https://localhost:3001/';
+const ENDPOINT = process.env.WS_ENDPOINT || 'http://localhost:3001/';
 
 const socket = ioClient(ENDPOINT);
 
