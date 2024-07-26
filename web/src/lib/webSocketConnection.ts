8df63be456f9f5c574fc6e322d40fef291ce85a2
Jo Kroese <jo@jokroese.com>
2024-07-26 12:53:05 +0200

    return to explicit endpoint

--- web/src/lib/webSocketConnection.ts
+++ web/src/lib/webSocketConnection.ts
@@ -1,9 +1,10 @@
 import ioClient from 'socket.io-client';
 
-const ENDPOINT = import.meta.env.VITE_WS_ENDPOINT;
-if (!ENDPOINT) {
-  throw new Error('VITE_WS_ENDPOINT is not set');
-}
+const ENDPOINT = 'https://ccg-api.stolenartifacts.org';
+// const ENDPOINT = import.meta.env.VITE_WS_ENDPOINT;
+// if (!ENDPOINT) {
+//   throw new Error('VITE_WS_ENDPOINT is not set');
+// }
 
 const socket = ioClient(ENDPOINT);
 console.log(`Communicating with websockets server at ${ENDPOINT}`);
