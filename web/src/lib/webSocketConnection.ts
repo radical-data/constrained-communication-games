d7e5638795ce41df9fda1377dd8d65c02c237b21
Jo Kroese <jo@jokroese.com>
2024-07-26 12:04:13 +0200

    fail visibly

--- web/src/lib/webSocketConnection.ts
+++ web/src/lib/webSocketConnection.ts
@@ -1,6 +1,9 @@
 import ioClient from 'socket.io-client';
 
-const ENDPOINT = import.meta.env.VITE_WS_ENDPOINT || 'http://localhost:3001';
+const ENDPOINT = import.meta.env.VITE_WS_ENDPOINT;
+if (!ENDPOINT) {
+  throw new Error('VITE_WS_ENDPOINT is not set');
+}
 const socket = ioClient(ENDPOINT);
 console.log(`Communicating with websockets server at ${ENDPOINT}`);
 export const io = socket;
