575e984b43adee7cef0eb41e5be599b8e2ac245b
Jo Kroese <jo@jokroese.com>
2024-07-26 12:04:03 +0200

    add vite prefix to environment variable

--- web/src/lib/webSocketConnection.ts
+++ web/src/lib/webSocketConnection.ts
@@ -1,9 +1,6 @@
 import ioClient from 'socket.io-client';
 
-const ENDPOINT = import.meta.env.VITE_WS_ENDPOINT || 'http://localhost:3001/';
-
+const ENDPOINT = import.meta.env.VITE_WS_ENDPOINT || 'http://localhost:3001';
 const socket = ioClient(ENDPOINT);
-
 console.log(`Communicating with websockets server at ${ENDPOINT}`);
-
 export const io = socket;
